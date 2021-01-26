41ca8b32d4cf428d0a78c912d7db59d24a5d2bed
AgistStudio <junior.ho@gmail.com>
2021-01-26 15:17:51 -0400

    Update gatsby-config.js
    added social sharing

--- gatsby-config.js
+++ gatsby-config.js
@@ -27,6 +27,16 @@ module.exports = {
         icon: `src/images/gatsby-icon.png`, // This path is relative to the root of the site.
       },
     },
+    `gatsby-plugin-social9-socialshare`,
+    {
+		resolve: `gatsby-plugin-social9-socialshare`,
+		options: {
+			content:  `xxxxxxxxxx`,
+			async:  true,
+			defer:  true
+		}
+	}],
+}
     // this (optional) plugin enables Progressive Web App + Offline functionality
     // To learn more, visit: https://gatsby.dev/offline
     // `gatsby-plugin-offline`,
